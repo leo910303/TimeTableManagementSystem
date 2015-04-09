e3db5b752fac384e8c122a18856e2e658a2d3c1a
leo910303 <it12102624@my.sliit.lk>
2015-04-09 23:14:45 +0530

    ASP.NET Folders
    Add ASP.NET Folders

---
+++ App_Code/Admin_Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeTableManagementSystem.App_Data;
+
+
+
+namespace TimeTableManagementSystem.App_Code
+{
+    public class Admin_Class
+    {
+        TimeTableManagementSystem.App_Data.User admin = new TimeTableManagementSystem.App_Data.User();
+        User user = new User();
+        LoginControl.Encryption login = new LoginControl.Encryption();
+                
+        public string Authentication(string Usrname, string Password)
+        {
+            try
+            {
+                return "";
+            }
+            catch
+            {
+                return "Not";
+            }
+        }
+    }
+}
