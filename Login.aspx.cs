6ecdf5f8667051f0505303af736fe8dddd9c6a2d
leo910303 <it12102624@my.sliit.lk>
2015-04-09 23:11:12 +0530

    Add Authentication and ASP.NET Folders

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,19 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using TimeTableManagementSystem.App_Data;
 namespace TimeTableManagementSystem_Update
 {
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         protected void btnSignIn_Click(object sender, EventArgs e)
         {
-            //Response.Redirect("Table.aspx",true);
+            TimeTableManagementSystem.App_Data.User us = new TimeTableManagementSystem.App_Data.User();
+            User user = new User();
+
+            var query = from contact in user.UserName
+                        where
+                            user.UserName == "Pavithra"
+                        select contact;
+
+            foreach (var result in query)
+            {
+                Response.Write(result);
+            }
+
+            //login.DecryptPassword(Password, "", "");
+
         }
     }
 }
